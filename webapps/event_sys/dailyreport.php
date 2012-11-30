297c118cacc10707c6eec6da82ab078896c98c59
yundu <yundu@anjuke.com>
2012-11-30 20:09:09 +0800

    update

--- webapps/event_sys/dailyreport.php
+++ webapps/event_sys/dailyreport.php
@@ -15,7 +15,7 @@ $rangeb = $today+63000;
 
 $todaydata = get_today_data($pdo,$rangea,$rangeb);
 
-$body = '<body>';
+$body = '';
 
 if (!$todaydata['critical'] && !$todaydata['nostop']){
     $body .= "恭喜，今日没有重大事件发生，您可以登录"."<a href='http://".$cfg['hostname']."/index.php'>事件管理系统</a>查看更多";
@@ -23,11 +23,11 @@ if (!$todaydata['critical'] && !$todaydata['nostop']){
 }
 else{
     if($todaydata['critical']){
-        $body .= "1)今日重大事件(事件等级<=P4)：<br /><table style='width:1050px;word-break:break-all;text-align: center;font-size: 15px;table-layout: fixed;border-top:1px solid #FFFFFF;border-bottom:1px solid #FFFFFF;border-left:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>
+        $body .= "1)今日重大事件(事件等级<=P4)：<br /><table style='width:1095px;word-break:break-all;text-align: center;font-size: 15px;table-layout: fixed;border-top:1px solid #FFFFFF;border-bottom:1px solid #FFFFFF;border-left:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>
                 <tr style='background-color:#4F81BC;color:white;'>
                     <td style='width:50px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>编号</th>
                     <td style='width:190px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>事件名称</th>
-                    <td style='width:100px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>事件等级</th>
+                    <td style='width:130px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>事件等级</th>
                     <td style='width:80px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>发生时间</th>
                     <td style='width:120px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>影响时长(分钟)</th>
                     <td style='width:170px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>事件影响</th>
@@ -41,8 +41,9 @@ else{
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'><a href='http://".$cfg['hostname']."/index.php?op=detail&eid=".$v['eid']."'>".$v['eid']."</a></td>
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>".$v['subject']."</td>
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>";
-    for($a=0;$a<$v['level'];$a++){$body.= "<img style='width:15px;height:15px;' border='0' src='http://vesta.corp.anjuke.com/images/level/reds.png'>";}
-    for($a=0;$a<(6-$v['level']);$a++){$body.= "<img style='width:15px;height:15px;' border='0' src='http://vesta.corp.anjuke.com/images/level/grays.png'>";}
+    if ($v['etypeid']=='6')$body.= "<img style='height:15px;' border='0' src='http://vesta.corp.anjuke.com/images/level/ls".(4-$v['level']).".png'>";
+    else $body.= "<img style='height:15px;' border='0' src='http://vesta.corp.anjuke.com/images/level/lv".(7-$v['level']).".png'>";
+
     $body .= "</td>
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>".date('H:i:s',$v['createtime'])."</td>
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>".$v['affecttime']."</td>
@@ -60,11 +61,11 @@ else{
     if($todaydata['nostop']){        
         $body .= "</table><br /><br /><br />
             2)未关闭事件当前进展：<br />
-<table style='width:1050px;word-break:break-all;text-align: center;font-size: 15px;table-layout: fixed;border-top:1px solid #FFFFFF;border-bottom:1px solid #FFFFFF;border-left:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>
+<table style='width:1095px;word-break:break-all;text-align: center;font-size: 15px;table-layout: fixed;border-top:1px solid #FFFFFF;border-bottom:1px solid #FFFFFF;border-left:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>
             <tr style='background-color:#4F81BC;color:white;'>
                 <td style='width:50px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>编号</th>
                 <td style='width:190px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>事件名称</th>
-                <td style='width:100px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>事件等级</th>
+                <td style='width:130px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>事件等级</th>
                 <td style='width:80px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>发生时间</th>
                 <td style='width:120px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>影响时长(分钟)</th>
                 <td style='width:170px;border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>事件影响</th>
@@ -78,8 +79,8 @@ else{
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'><a href='http://".$cfg['hostname']."/index.php?op=detail&eid=".$v['eid']."'>".$v['eid']."</a></td>
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>".$v['subject']."</td>
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>";
-    for($a=0;$a<$v['level'];$a++){$body.= "<img style='width:15px;height:15px;' border='0' src='http://vesta.corp.anjuke.com/images/level/reds.png'>";}
-    for($a=0;$a<(6-$v['level']);$a++){$body.= "<img style='width:15px;height:15px;' border='0' src='http://vesta.corp.anjuke.com/images/level/grays.png'>";}
+    if ($v['etypeid']=='6')$body.= "<img style='height:15px;' border='0' src='http://vesta.corp.anjuke.com/images/level/ls".(4-$v['level']).".png'>";
+    else $body.= "<img style='height:15px;' border='0' src='http://vesta.corp.anjuke.com/images/level/lv".(7-$v['level']).".png'>";
     $body .= "</td>
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>".date('H:i:s',$v['createtime'])."</td>
                     <td style='border-bottom:1px solid #FFFFFF;border-right:1px solid #FFFFFF;'>".$v['affecttime']."</td>
@@ -90,7 +91,7 @@ else{
     }
 
 
-    $body .= "</table></body>";
+    $body .= "</table>";
 }
 $subject = "安居客事件管理日报";
 $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
@@ -98,6 +99,7 @@ $smtp = new smtp($cfg['smtp']['server'],$cfg['smtp']['port'],true,$cfg['smtp']['
 $email_arr = array(
         //'6' => 'fzhou@anjuke.com',
         '1' => 'yundu@anjuke.com',
+        //'2' => 'cimena1989@163.com',
 );
 /*$email_arr = array(
         '0' => 'dl-tech-ops@anjuke.com',
